0ff422f123bcd0e98df4777609eb69fcd381ef8e
Otávio <otavio.brito@aluno.colegioplanck.com.br>
2023-11-15 18:35:17 -0300

    Correção da Navbar

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -46,11 +46,41 @@
                 <a class="text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500 hover:text-green-400"
                     href="/">Home</a>
                 <a class="text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500 hover:text-green-400"
-                    href="{{ route('categories.index') }}">Categories</a>
+                    href="{{ route('categories.index') }}">Categorias</a>
                 <a class="text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500 hover:text-green-400"
-                    href="{{ route('menus.index') }}">Our Menu</a>
-                <a class="text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500 hover:text-green-400"
-                    href="{{ route('reservations.step.one') }}">Make Reservation</a>
+                    href="{{ route('menus.index') }}">Nosso menu</a>
+                <div @click.away="open = false" class="relative" x-data="{ open: false }">
+                    <button @click="open = !open"
+                        class="text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500 hover:text-green-400">
+                        <span>Reservas</span>
+                        <svg fill="#50df8d" viewBox="0 0 20 20" :class="{ 'rotate-180': open, 'rotate-0': !open }"
+                            class="inline w-4 h-4 mt-1 ml-1 transition-transform duration-200 transform md:-mt-1">
+                            <path fill-rule="evenodd"
+                                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                clip-rule="evenodd"></path>
+                        </svg>
+                    </button>
+                    <div x-show="open" x-transition:enter="transition ease-out duration-100"
+                        x-transition:enter-start="transform opacity-0 scale-95"
+                        x-transition:enter-end="transform opacity-100 scale-100"
+                        x-transition:leave="transition ease-in duration-75"
+                        x-transition:leave-start="transform opacity-100 scale-100"
+                        x-transition:leave-end="transform opacity-0 scale-95"
+                        class="absolute right-0 w-full mt-2 origin-top-right rounded-md shadow-lg">
+                        <div class="px-2 py-2 bg-white rounded-md shadow" style="width: 216px;">
+                            <x-dropdown-link href="{{ route('reservations.step.one') }}"
+                                class="text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500 hover:text-green-400"
+                                style="font-size: 15px;padding-left: 15px;">
+                                {{ __('Faça a sua reserva') }}
+                            </x-dropdown-link>
+                            <x-dropdown-link href="{{ route('reservations.check.form') }}"
+                                class="text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500 hover:text-green-400"
+                                style="font-size: 15px;padding-left: 15px;">
+                                {{ __('Situação da reserva') }}
+                            </x-dropdown-link>
+                        </div>
+                    </div>
+                </div>
 
             </div>
         </nav>
